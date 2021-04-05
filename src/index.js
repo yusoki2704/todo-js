2032b17106fd4b3ce943bfaccafdb93a1efe00d3
yusoki2704 <71821574+yusoki2704@users.noreply.github.com>
2021-04-05 19:22:33 +0900

    add complete button and delete button

--- src/index.js
+++ src/index.js
@@ -17,8 +17,24 @@ const onClickAdd = () => {
   p.appendChild(document.createTextNode(inputText));
   p.className = "todo-item";
 
+  //button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button(完了)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
   // divタグの子要素に各要素を設定
   div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // liタグの子要素に各要素を設定
   li.appendChild(div);
